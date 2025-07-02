2b18ceb044a35669f3784dc5343f5738167dd819
sohanur28 <gsrahman28@gmail.com>
2025-07-02 11:53:08 +0600

    promise

--- async.js
+++ async.js
@@ -1,13 +1,46 @@
-console.log(1);
-console.log(2);
-setTimeout(() => {
-    console.log('lazy logged')
-}, 4000)
-console.log(4);
-console.log(5);
-console.log(6);
+// console.log(1);
+// console.log(2);
+// setTimeout(() => {
+//     console.log('lazy logged')
+// }, 4000)
+// console.log(4);
+// console.log(5);
+// console.log(6);
 
-function doSomething(){
-    console.log(3);
+// function doSomething(){
+//     console.log(3);
     
+// }
+
+const myLoader = () => {
+    return new Promise((resolve, reject) =>{
+        const success = Math.random();
+        if(success <= 0.5){
+            resolve(success)
+        }
+        else{
+            reject(success)
+        }
+    })
 }
+
+myLoader()
+.then(data => console.log('resolved data', data))
+.catch(err => console.log('rejected with value', err))
+
+fetch('')
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log(err))
+
+async function loadData(){
+    const res = await fetch('');
+    const data = await res.json();
+    console.log(data); 
+}
+
+const taskLoader = async() =>{
+    const res = await fetch('');
+}
+
+loadData();
